a9ffa440eabc57e9ca67f154308be8605ff0ad9c
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 15:06:23 +0900

    기초작업

---
+++ app/src/main/java/com/example/androidbaisic03_20200521/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidbaisic03_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
