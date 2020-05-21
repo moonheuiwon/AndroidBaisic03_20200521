73b1d0a1c7f47adf833587280c25f03f990a9fdd
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 16:40:17 +0900

    돌아갈 데이터 세팅, 액티비티 종료

--- app/src/main/java/com/example/androidbaisic03_20200521/EditUserPhoneActivity.java
+++ app/src/main/java/com/example/androidbaisic03_20200521/EditUserPhoneActivity.java
@@ -1,21 +1,43 @@
 package com.example.androidbaisic03_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.androidbaisic03_20200521.databinding.ActivityEditUserNickNameBinding;
+import com.example.androidbaisic03_20200521.databinding.ActivityEditUserPhoneBinding;
 
 public  class EditUserPhoneActivity extends BaseActivity {
+
+    ActivityEditUserPhoneBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_phone);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_user_phone);
         setupEvents();
         setValues();
     }
 
     @Override
     public void setupEvents() {
+
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputPhoneNum = binding.phoneNumEdt.getText().toString();
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("phone", inputPhoneNum);
+
+                setResult(RESULT_OK, resultIntent);
+
+                finish();
+            }
+        });
 
     }
 
