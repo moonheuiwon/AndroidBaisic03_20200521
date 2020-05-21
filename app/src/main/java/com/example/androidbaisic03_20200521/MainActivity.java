246757c29d2a5a41c5fea912a58eb937e6ee064d
moonheuiwon <wonheuimoon@gmail.com>
2020-05-21 16:20:04 +0900

    폰번 수정 화면 호출  - StartActivityForResult  어떤 요청들인지 멤버변수에 담아서 요청

--- app/src/main/java/com/example/androidbaisic03_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbaisic03_20200521/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends BaseActivity {
 
     ActivityMainBinding binding;
 
+    final  int REQ_FOR_NICKNAME = 1001;
+    final int REQ_FOR_PHONE_NUM = 1002;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +28,20 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvents() {
 
+        binding.editPhoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, EditUserPhoneActivity.class);
+                startActivityForResult(myIntent, REQ_FOR_PHONE_NUM);
+            }
+        });
+
 //        대부분 변수가 똑같을때 변수로 만들어서 사용한다.
         View.OnClickListener ocl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent myIntent = new Intent(mContext, EditUserNickNameActivity.class);
-                startActivityForResult(myIntent, 1001);
+                startActivityForResult(myIntent, REQ_FOR_NICKNAME);
             }
         };
 
@@ -47,7 +58,7 @@ public class MainActivity extends BaseActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == 1001) {
+        if (requestCode == REQ_FOR_NICKNAME) {
             if (resultCode == RESULT_OK) {
 
                 String nick = data.getStringExtra("editNickName");
